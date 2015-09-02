e17d21d8b2e6f8185a051982c9898e69a375dd96
wp.cbos.ca <wp@cbos.ca>
2015-09-02 16:15:29 -0400

    avatars disabled (yeah!).

--- wp-content/plugins/wp-installer/settings/discussion/discussion.php
+++ wp-content/plugins/wp-installer/settings/discussion/discussion.php
@@ -6,7 +6,7 @@ function install_discussion_settings() {
     require_once dirname( __FILE__) . '/data.php';
     $items = get_discussion_data();
     if ( ! empty ( $items ) ) foreach ( $items as $item ) {
-        if( $item['update'] ) {
+        if( ! $item['update'] ) {
             update_option( $item['option_name'], $item['option_value'] );
         }
     }
