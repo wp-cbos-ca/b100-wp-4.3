73c7388dd37fdec0c1d31052ebfc36417416c53b
wp.cbos.ca <wp@cbos.ca>
2015-09-02 14:50:35 -0400

    Updated install tags function (very simple).

--- wp-content/plugins/wp-installer/tags/data.php
+++ wp-content/plugins/wp-installer/tags/data.php
@@ -3,7 +3,7 @@
 defined( 'ABSPATH' ) || die();
 
  function get_tags_data(){
-     $items = array( 'tag' );
+     $items = array( 'tag-01', 'tag-02', 'tag-03', 'tag-04', 'tag-05' );
      return $items;
  }
  
--- wp-content/plugins/wp-installer/tags/tags-assign/tags-assign.php
+++ wp-content/plugins/wp-installer/tags/tags-assign/tags-assign.php
@@ -2,3 +2,47 @@
 
 defined( 'ABSPATH' ) || die();
 
+function assign_tags_to_posts() {
+    require_once dirname( __FILE__) . '/data.php';
+    $tags = get_tags_data();
+    if ( $posts = get_posts_by_post_type( WP_POST_TYPE, 15 ) ) {
+        foreach ( $posts as $post ) {
+            generate_tag_data( $post, $tags );
+        }
+    }
+}
+
+function generate_tag_data( $post, $tags ) {
+    $tag = $tags[ rand( 0, count( $tags ) - 1 ) ];
+    $tag .= rand( 1,3 );
+    wp_set_post_tags( $post -> ID, $tag, false ); 
+}
+
+function get_posts_by_post_type( $post_type = 'post', $numposts = '' ) {
+    $args = array(
+        'posts_per_page'   => $numposts,
+        'offset'           => 0,
+        'category'         => '',
+        'category_name'    => '',
+        'orderby'          => 'date',
+        'order'            => 'DESC',
+        'include'          => '',
+        'exclude'          => '',
+        'meta_key'         => '',
+        'meta_value'       => '',
+        'post_type'        => $post_type,
+        'post_mime_type'   => '',
+        'post_parent'      => '',
+        'author'       => '',
+        'post_status'      => 'publish',
+        'suppress_filters' => true 
+    );
+    $arr = get_posts( $args ); 
+    if ( ! empty ( $arr ) ) {
+        return $arr;
+    } 
+    else {
+        return false;
+    }
+}
+
