2b66aea7eedf743930e39dfc1ba1db12a820141b
wp.cbos.ca <wp@cbos.ca>
2015-10-08 18:51:12 -0400

    update
    confusing.

--- wp-content/plugins/wp-site-installer/widgets/widgets.php
+++ wp-content/plugins/wp-site-installer/widgets/widgets.php
@@ -3,6 +3,7 @@
 defined( 'ABSPATH' ) || die();
 
 function configure_widgets() {
+    wp_cache_delete ( 'alloptions', 'options' );
     require_once( dirname(__FILE__) . '/data.php' );
     $theme = get_widget_theme_data();
     if ( $theme['name'] == get_option('template' ) ) {
@@ -12,11 +13,24 @@ function configure_widgets() {
 }
 
 function update_sidebars_widgets() {  
-    $items = get_sidebars_widget_data();
+    $widgets = get_sidebars_widget_data();
     $existing = get_option( 'sidebars_widgets' );
-    $updated = build_sidebars_widgets_array( $existing, $items );
-    update_option( 'sidebars_widgets', $updated );
+    wdump( $existing, 0 );
+    update_option( 'sidebars_widgets', $widgets );
+    $new = get_option( 'sidebars_widgets' );
+    wdump( $new, 0 );
 } 
+
+function update_widgets(){
+    $search = get_search_widget_data();
+    $existing = get_option( 'widget_search' );
+    wdump( $existing, 0 );
+    if ( $search['update'] ) {        
+        update_option( 'widget_search', $search['widget'] );    
+    }    
+    $new = get_option( 'widget_search' );
+    wdump( $new, 0 );
+}
 
 function build_sidebars_widgets_array( $existing, $items ){
     foreach ( $items as $name => $item ) {
@@ -27,15 +41,11 @@ function build_sidebars_widgets_array( $existing, $items ){
     return $existing;
 }
 
-function update_widgets(){
-    $search = get_search_widget_data();
-    if ( $search['update'] ) {        
-        update_option( 'widget_search', $search['widget'] );    
-    }    
-}
 
-function wdump( $arr ) {
-    echo "<pre>";
-    var_dump( $arr );
-    echo "</pre>";
+function wdump( $arr, $output = 1 ) {
+    if ( $output ) {
+        echo "<pre>";
+        var_dump( $arr );
+        echo "</pre>";
+    }
 }
