f23741a7076e2025fcd77bc02e6ca533ac1477a1
wp.cbos.ca <wp@cbos.ca>
2015-10-08 20:12:42 -0400

    update (minor).

--- wp-content/plugins/wp-site-installer/widgets/widgets.php
+++ wp-content/plugins/wp-site-installer/widgets/widgets.php
@@ -18,22 +18,18 @@ function configure_widgets() {
 function update_sidebars_widgets() {  
     $widgets = get_sidebars_widget_data();
     $existing = get_option( 'sidebars_widgets' );
-    wdump( $existing, 1 );
     update_option( 'sidebars_widgets', $widgets );
     $new = get_option( 'sidebars_widgets' );
-    wdump( $new, 1 );
 } 
 
 function update_widgets(){
     $search = get_search_widget_data();
     $existing = get_option( 'widget_search' );
-    wdump( $existing, 0 );
     update_option( 'widget_search', '' );
     if ( $search['update'] ) {        
         update_option( 'widget_search', $search['widget'] );    
     }    
     $new = get_option( 'widget_search' );
-    wdump( $new, 0 );
 }
 
 function build_sidebars_widgets_array( $existing, $items ){
