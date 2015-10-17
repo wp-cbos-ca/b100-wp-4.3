ed11ad5ee1dff9a9a80d812540870f65277dc15d
wp.cbos.ca <wp@cbos.ca>
2015-10-17 14:12:42 -0400

    update

--- wp-content/plugins/restrained-social/data.php
+++ wp-content/plugins/restrained-social/data.php
@@ -4,11 +4,11 @@ defined( 'ABSPATH' ) || die();
 
 function get_social_data() {
     $items = array( 
-            0 => array( 'name' => 'twitter', 'title' => 'Twitter', 'display' => 1 ),
-            1 => array( 'name' => 'facebook',  'title' => 'Facebook', 'display' => 1 ),
-            2 => array( 'name' => 'rss',  'title' => 'RSS', 'display' => 0 ),
-            3 => array( 'name' => 'linkedin',  'title' => 'LinkedIn', 'display' => 1 ),
-            4 => array( 'name' => 'googleplus', 'title' => 'Google+', 'display' => 1 )
+            array( 'name' => 'twitter', 'title' => 'Twitter', 'display' => 1 ),
+            array( 'name' => 'facebook',  'title' => 'Facebook', 'display' => 1 ),
+            array( 'name' => 'linkedin',  'title' => 'LinkedIn', 'display' => 1 ),
+            array( 'name' => 'googleplus', 'title' => 'Google+', 'display' => 1 ),
+            array( 'name' => 'instagram', 'title' => 'Instagram', 'display' => 0 ),
         );
         return $items;                
 }
