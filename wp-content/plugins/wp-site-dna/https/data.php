ac6b4c22948f82cc8d27957f5a4724db801238bb
wp.cbos.ca <wp@cbos.ca>
2015-10-20 09:06:44 -0400

    update

--- wp-content/plugins/wp-site-dna/https/data.php
+++ wp-content/plugins/wp-site-dna/https/data.php
@@ -3,14 +3,18 @@
 defined( 'ABSPATH' ) || die();
 
 function get_https_data(){
-    $items = array( 'https' => 1 );
+    $items = array( 
+        'https' => 1,
+        'subdomain' => 1,
+        'www' => 0,
+        );
     return $items;
 }
 
 function get_https_on_arr(){
     $arr = array( 
     '<IfModule mod_rewrite.c>',
-    '  # RewriteEngine On',
+    '  RewriteEngine On',
     '  # RewriteCond %{HTTP_HOST} !^www\.',
     '  # RewriteRule ^(.*)$ https://www.%{HTTP_HOST}/$1 [R=301,L]',
     '  RewriteCond %{HTTPS} off',
