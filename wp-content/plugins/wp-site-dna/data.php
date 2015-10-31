a9b4a40fde8912e538c88d40be72337aadc8da0a
wp.cbos.ca <wp@cbos.ca>
2015-10-31 15:40:25 -0400

    update

--- wp-content/plugins/wp-site-dna/data.php
+++ wp-content/plugins/wp-site-dna/data.php
@@ -4,11 +4,11 @@ defined( 'ABSPATH' ) || die();
 
 function get_installer_run_data(){
     $items = array(
-        'site_one'  => array ( 'run' => 0, 'ran' => 0 ),
+        'site_one'  => array ( 'run' => 1, 'ran' => 0 ),
         'site_two'  => array ( 'run' => 1, 'ran' => 0 ),
-        'settings'  => array ( 'run' => 0, 'ran' => 0 ),
-        'content'   => array ( 'run' => 0, 'ran' => 0 ),
-        'images'    => array ( 'run' => 0, 'ran' => 0 ),
+        'settings'  => array ( 'run' => 1, 'ran' => 0 ),
+        'content'   => array ( 'run' => 1, 'ran' => 0 ),
+        'images'    => array ( 'run' => 1, 'ran' => 0 ),
         'sorting'   => array ( 'run' => 0, 'ran' => 0 ),
     );
     return $items;
@@ -20,17 +20,17 @@ function get_site_one_data(){
         array( 'name' => 'dashboard',   'run' => 1, 'ran' => 0 ),
         array( 'name' => 'themes',      'run' => 1, 'ran' => 0 ),
         array( 'name' => 'plugins',     'run' => 1, 'ran' => 0 ),        
-        array( 'name' => 'users',       'run' => 0, 'ran' => 0 ),
+        array( 'name' => 'users',       'run' => 1, 'ran' => 0 ),
     );
     return $items;
 }
 
 function get_site_two_data(){
     $items = array(
-        array( 'name' => 'clean',       'run' => 0, 'ran' => 0 ),
+        array( 'name' => 'clean',       'run' => 1, 'ran' => 0 ),
         array( 'name' => 'menus',       'run' => 1, 'ran' => 0 ),
-        array( 'name' => 'menu-assign', 'run' => 0, 'ran' => 0 ),
-        array( 'name' => 'widgets',     'run' => 0, 'ran' => 0 ),
+        array( 'name' => 'menu-assign', 'run' => 1, 'ran' => 0 ),
+        array( 'name' => 'widgets',     'run' => 1, 'ran' => 0 ),
         );
     return $items;
 }
@@ -54,7 +54,7 @@ function get_content_data(){
         array( 'name' => 'page-block',      'run' => 0, 'ran' => 0 ),
         array( 'name' => 'posts',           'run' => 1, 'ran' => 0 ),
         array( 'name' => 'posts-block',     'run' => 0, 'ran' => 0 ),
-        array( 'name' => 'post-type',       'run' => 0, 'ran' => 0 ),
+        array( 'name' => 'post-type',       'run' => 1, 'ran' => 0 ),
         array( 'name' => 'post-type-block', 'run' => 0, 'ran' => 0 ),
     );
     return $items;
